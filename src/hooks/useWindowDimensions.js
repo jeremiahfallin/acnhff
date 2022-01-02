b591b707775fefa29fe9317fb6b4663851123140
Jeremiah <jeremiah.fallin@gmail.com>
2022-01-02 15:52:21 -0800

    window undefined in gatsby build

--- src/hooks/useWindowDimensions.js
+++ src/hooks/useWindowDimensions.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
+  if (!window) return { width: 0, height: 0 };
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
-    height
+    height,
   };
 }
 
